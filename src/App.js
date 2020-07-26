c8b809fe5664968ea543423b8598ca05eea71e5f
wenzstev <wenzelstev@gmail.com>
2020-07-26 17:24:56 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
 import ListPage from "./components/Pages/ListPage/"
 import LoginPage from "./components/Pages/LoginPage/"
+import HomePage from "./components/Pages/HomePage/"
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/lists">
+            <ListPage />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+
   );
 }
 
