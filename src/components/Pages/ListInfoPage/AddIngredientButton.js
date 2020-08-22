762e0fef79780f69399b33ebc2355c9f0312134b
wenzstev <wenzelstev@gmail.com>
2020-08-22 16:11:34 -0400

    more work on adding individual ingredients (harder than expected)

--- src/components/Pages/ListInfoPage/AddIngredientButton.js
+++ src/components/Pages/ListInfoPage/AddIngredientButton.js
@@ -9,15 +9,16 @@ import {
   Input
 } from "@material-ui/core"
 
+import {
+  useParams
+} from "react-router-dom"
+
 import axios from "../../../AxiosConfig"
 
-const AddIngredientButton = () => {
+const AddIngredientButton = (props) => {
   const [inputOpen, setInputOpen] = useState(false)
   const [newIngredient, setNewIngredient] = useState("")
 
-  const checkIfAdditionalIngredientsRecipe = () => {
-
-  }
 
   console.log(newIngredient)
   return (
@@ -43,11 +44,56 @@ const ClosedAddIngredient = (props) => {
 }
 
 const OpenAddIngredient = (props) => {
+  const {listId} = useParams()
+  const checkIfAdditionalIngredientsRecipe = () => {
+    axios.get(`/lists/${props.listId}/additionalingredients`)
+    .then((response)=>{
+
+    })
+    .catch((err)=>{
+      if(err.response){
+        if(err.response.status == 404){
+          console.log("no additional ingredients found")
+          return axios.post(`/recipes`,{
+            name: "Additional Ingredients",
+          })
+        } else {
+          throw new Error("Server returned with error: " + err.response.status)
+        }
+      }
+    })
+    .then((response)=>{
+      if(response.status == 201){
+        return axios.post(`/list-recipe-associatons`,{
+          recipe_id: response.body.recipe.id,
+          grocerylist_id: listId
+        })
+      }
+    })
+    .then((response)=>{
+      if(response.status == 201){
+        return axios.post(`/lines`,{
+          recipe_id: response.body.recipe.id,
+          text: "test text",
+          ingredients: []
+        })
+      }
+    })
+    .then((response)=>{
+      if(response.status = 201){
+        return axios.put(`/lines/${response.body.line.id}/ingredients`,{
+          new_ingredients:[
+            
+          ]
+        })
+      }
+    })
+  }
 
   return (
     <Box>
       <Input value={props.newIngredient} onChange={(e)=>props.setNewIngredient(e.target.value)}/>
-      <ButtonBase><CheckCircleIcon /></ButtonBase>
+      <ButtonBase onClick={checkIfAdditionalIngredientsRecipe}><CheckCircleIcon /></ButtonBase>
       <ButtonBase onClick={()=>props.setInputOpen(false)}><RemoveCircleIcon /></ButtonBase>
     </Box>
   )
